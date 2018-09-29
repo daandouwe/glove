d38ba9cf9542b19382486ac974c6cdbbda1deb67
daandouwe <daan_douwe@outlook.com>
2018-09-29 22:32:12 +0200

    Sparse matrix tests.

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -1,5 +1,9 @@
+"""
+Test various sparse matrices for speed.
+"""
 import time
 import sys
+
 import numpy as np
 from scipy.sparse import dok_matrix, csr_matrix, csc_matrix, coo_matrix, lil_matrix
 
@@ -73,7 +77,6 @@ def index_test(matrix_type, k, n):
         X = construct_csr(n, m=m)
     else:
         X = construct_csr(n, m=m)
-
     t0 = time.time()
     for _ in range(k):
         ix = np.random.randint(0, high=n, size=(128,))
@@ -81,8 +84,8 @@ def index_test(matrix_type, k, n):
         x = X[submat].todense()
     avg = (time.time() - t0) / k
     print('{}: {} seconds'.format(matrix_type, avg))
-
     return avg
+
 
 def index_test_array(k, n):
     X = np.zeros((n,n), dtype=np.float32)
@@ -98,20 +101,19 @@ def index_test_array(k, n):
 
 if __name__ == '__main__':
     n = 1000
-
     if len(sys.argv) > 1:
         n = int(sys.argv[1])
 
     print('Testing construction...')
-    avg = construct_test(10, n, m=n, matrix='dok')
-    avg = construct_test(10, n, m=n, matrix='lil')
-    avg = construct_test(10, n, m=n, matrix='coo')
-    avg = construct_test(10, n, m=n, matrix='csc')
-    avg = construct_test(10, n, m=n, matrix='csr')
+    construct_test(10, n, m=n, matrix='dok')
+    construct_test(10, n, m=n, matrix='lil')
+    construct_test(10, n, m=n, matrix='coo')
+    construct_test(10, n, m=n, matrix='csc')
+    construct_test(10, n, m=n, matrix='csr')
     print()
 
-    print('Testing indexing')
-    avg = index_test_array(100, n)
-    # avg = index_test('dok', 100)
-    avg = index_test('csr', 100, n)
-    avg = index_test('csc', 100, n)
+    print('Testing indexing...')
+    index_test_array(100, n)
+    index_test('csr', 100, n)
+    index_test('csc', 100, n)
+    # index_test('dok', 100)
